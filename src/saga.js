06db3588dff74fc415ad06000bc65bffe4cbe87d
jsfix.live analysis <info@coana.tech>
2022-07-22 09:59:56 +0200

    JSFIX patches

--- src/saga.js
+++ src/saga.js
@@ -34,7 +34,10 @@ export function* routeSaga(routesMap) {
 		if (currentNavigateTask) {
 			const _t = currentNavigateTask;
 			currentNavigateTask = null;
-			yield cancel(_t);
+			yield /* TODO: JSFIX could not patch the breaking change:
+            errors thrown during cancellation process are no longer swallowed, you need to keep finally blocks fail-safe
+            Suggested fix: Code in the finally block of the cancelled task should be wrapped in a try-catch or similar if errors may appear in the finally block (NOTICE, in many cases, sufficient error handling is already in place).*/
+            cancel([_t]);
 		}
 	}
 	function* forkNavigateTask(route, params, query) {
@@ -46,7 +49,10 @@ export function* routeSaga(routesMap) {
 		if (currentQueryTask) {
 			const _t = currentQueryTask;
 			currentQueryTask = null;
-			yield cancel(_t);
+			yield /* TODO: JSFIX could not patch the breaking change:
+            errors thrown during cancellation process are no longer swallowed, you need to keep finally blocks fail-safe
+            Suggested fix: Code in the finally block of the cancelled task should be wrapped in a try-catch or similar if errors may appear in the finally block (NOTICE, in many cases, sufficient error handling is already in place).*/
+            cancel([_t]);
 		}
 	}
 	function* forkQueryTask(route, params, query) {
